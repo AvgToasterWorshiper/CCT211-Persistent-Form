e4d54be230c8408b9844768859eea5c81232efe6
The Pixel Polygon <ThePixelPolygon@users.noreply.github.com>
2023-04-05 21:56:22 -0400

    login working

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from tkinter import *
 from tkinter import messagebox
+import sqlite3
 
 class Session:
     def __init__(self, root, userID=None):
         self.current_user = userID
         self.root = root
+        self.connection = sqlite3.connect("./Persistent Inventory Management System.db")
+        self.attempt_user = StringVar()
+        self.attempt_pass = StringVar()
 
     def logout(self):
         pass
@@ -13,10 +17,29 @@ def quit(inst):
     inst.root.quit()
 
 def login(inst):
-    # This function should always be referencing a "logged-out" instance
+    """
+    Check to see if user from entry exits, if so login and switch to new instance
 
-    # Check to see if user from entry exits, if so login and switch to new instance
-    pass
+    This function should always reference a logged-out instance
+    :param inst:
+    :return:
+    """
+    if inst.current_user is None:
+        cur = inst.connection.cursor()
+        user = cur.execute("SELECT UserPassword FROM Users WHERE UserID IS ?", (inst.attempt_user.get(),))
+        password_fetch = user.fetchone()
+        password = None
+        if password_fetch is not None:
+            password = password_fetch[0]
+
+        if inst.attempt_pass.get() == password:
+            inst.current_user = cur.execute("SELECT UserID FROM Users WHERE UserID IS ?",
+                                            (inst.attempt_user.get(),)).fetchone()[0]
+            inst.attempt_user.set("")
+            inst.attempt_pass.set("")
+        else:
+            messagebox.showerror(title="Login Failed", message="Incorrect username and/or password.")
+
 
 def create_menus(inst):
     top = Menu(inst.root)
@@ -56,8 +79,8 @@ def create_menus(inst):
         login_userpassword.pack(side='top', fill=BOTH, expand=1)
 
         login_frame_right = Frame(login_frame)
-        userid_entry = Entry(login_frame_right, font=("Arial", 12))
-        userpassword_entry = Entry(login_frame_right, font=("Arial", 12), show="*")
+        userid_entry = Entry(login_frame_right, font=("Arial", 12), textvariable=inst.attempt_user)
+        userpassword_entry = Entry(login_frame_right, font=("Arial", 12), show="*", textvariable=inst.attempt_pass)
         userid_entry.pack(side='top', fill=BOTH, expand=1, padx=40, pady=40)
         userpassword_entry.pack(side='top', fill=BOTH, expand=1, padx=40, pady=40)
 
