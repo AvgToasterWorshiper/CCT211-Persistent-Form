9b1aa0c5a7e2e5e07f78f3e5a82c81648a932cd5
Cozmic <57224065+AvgToasterWorshiper@users.noreply.github.com>
2023-04-09 00:09:49 -0400

    "Fixed" filtering LOL

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ from typing import List, Any
 
 def filter_items(connection: Connection, filter_id: str, filter_name: str) -> List[Any]:
     cur = connection.cursor()
-    items = cur.execute("SELECT * FROM items WHERE 'id' LIKE ? AND 'name' LIKE ?",
+    items = cur.execute("SELECT * FROM items WHERE id LIKE ? AND name LIKE ?",
                         (f"%{filter_id}%", f"%{filter_name}%"))
     return items.fetchall()
 
